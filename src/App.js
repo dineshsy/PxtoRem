7f0995deb8f23cd63f5ce74539c97f5d58d683a6
dineshsy <s.ydinesh1177@gmail.com>
2020-08-22 19:18:38 +0530

    Px to Rem Chrome Extension

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 /*global chrome*/
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    defaultValue: 16,
+    inputPx: 1,
+    result: 16,
+  };
+
+  calculatePxtoRem = (defaultValue, inputPx) => {
+    let { result } = { ...this.state };
+    result = +inputPx / +defaultValue;
+    return result;
+  };
+
+  handleInputChange = (event) => {
+    if (event.target.id === "default-value") {
+      this.setState({ defaultValue: event.target.value });
+    } else if (event.target.id === "input-value") {
+      this.setState({ inputPx: event.target.value });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-          :
-            <img src={logo} className="App-logo" alt="logo" />
-          }
-
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">PxtoRem</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="form-container">
+          <div className="input-container">
+            <div class="input-field">
+              <input
+                onChange={this.handleInputChange}
+                value={this.state.defaultValue}
+                type="number"
+                id="default-value"
+              />
+              <label for="name">Default pixel size:</label>
+            </div>
+            px
+          </div>
+          <div className="input-container">
+            <div class="input-field">
+              <input
+                onChange={this.handleInputChange}
+                value={this.state.inputPx}
+                type="number"
+                id="input-value"
+              />
+              <label for="name">Pixels:</label>
+            </div>
+            px
+          </div>
+        </div>
+        <div style={{ justifyContent: "center" }} className="input-container">
+          {this.calculatePxtoRem(this.state.defaultValue, this.state.inputPx)}
+          rem
+        </div>
       </div>
     );
   }
